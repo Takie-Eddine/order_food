d5ad27cfd338361eebace841fbb6e2935adc2526
yacoubdemire <yacoubdemire@gmail.com>
2022-12-03 09:50:08 +0100

    add rest

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -16,7 +16,7 @@ class DashboardController extends Controller
         $meals = Order::selectRaw('SUM(total) as total')->whereMonth('created_at',Carbon::now()->month())->first();
         $cash_in = Expence_details::selectRaw('SUM(price) as total')->where('type' , '=' , 'cash_in')->first();
         $cash_out = Expence_details::selectRaw('SUM(price) as total')->where('type' , '=' , 'cash_out')->first();
-        $rest = Expence_details::select('total')->latest()->first();
+        $rest = Expence_details::select('total')->orderBy('created_at', 'desc')->first();
         return view('dashboard.index',compact('meals','cash_in','cash_out','rest'));
     }
 }
